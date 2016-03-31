041bab68be62c26ee4b4ce586971a570e9039784
Austin Turner <paustint@gmail.com>
2016-03-31 08:37:33 -0600

    get raw body

--- app.js
+++ app.js
@@ -28,9 +28,22 @@
     // uncomment after placing your favicon in /public
     //app.use(favicon(__dirname + '/public/favicon.ico'));
     app.use(logger('dev'));
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(xmlParser());
+
+    app.use(function(req, res, next) {
+      req.rawBody = '';
+      req.setEncoding('utf8');
+
+      req.on('data', function(chunk) { 
+        req.body += chunk;
+      });
+
+      req.on('end', function() {
+        next();
+      });
+    });
+    // app.use(bodyParser.json());
+    // app.use(bodyParser.urlencoded({ extended: false }));
+    // app.use(xmlParser());
     app.use(cookieParser());
     app.use(express.static(path.join(__dirname, 'public')));
 
